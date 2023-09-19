31cb430aaf2ade4994fd3ed1712875eee2eb36a1
charleyf <charleyferguson@navapbc.com>
2023-09-19 11:16:16 -0400

    Add a new component for the UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import useAcuantSDK from './acuant-handling/useAcuantSDK';
+import AcuantCameraUI from './acuant-handling/AcuantCameraUI';
 
 function App() {
   useAcuantSDK()
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AcuantCameraUI />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the MVP of the AcuantSDK (09/19/2023)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/acuant-handling/AcuantCameraUI.js
@@ -0,0 +1,4 @@
+const AcuantCameraUI = () => {
+
+}
+export default AcuantCameraUI;
